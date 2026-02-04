68748e2a3c040698f33a5ceb6085fe8871633ea9
zyplocs <zyplocked@gmail.com>
2026-02-03 18:16:50 -0600

    Transplant a practice view

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -1,5 +1,5 @@
 //
-//  PracticeView.swift
+//  ContentView.swift
 //  BetterRest
 //
 //  Created by Eli J on 2/3/26.
@@ -7,12 +7,44 @@
 
 import SwiftUI
 
-struct PracticeView: View {
+struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents:
+                        .hourAndMinute)
+                .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+                    .buttonStyle(GlassButtonStyle())
+            }
+        }
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
 #Preview {
-    PracticeView()
+    ContentView()
 }
