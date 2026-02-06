9dd8ea644989bffa5d5b0b709479048a6c622816
zyplocs <zyplocked@gmail.com>
2026-02-05 23:58:59 -0600

    Continue work on bedtime calc function & include future alerts

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,6 +13,10 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -46,7 +50,15 @@ struct ContentView: View {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
             
-            // more to come...
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
         } catch {
             // Something went wrong!!
         }
